b6f15a0ec85ba12da38fa165240d7daefb6b2830
Ennio <enniolopes@gmail.com>
2016-04-23 20:45:39 -0300

    Organize Introduction to R files and folders
    All data needed to Introduction to R were (and will be) included inside
    'datasets' folder.

---
+++ Introduction to R/2. Getting Data In/2_getting_data.R
@@ -0,0 +1,173 @@
+# Introduction to R #
+
+#2. GETTING DATA IN_________________________________
+
+#There are a few principal functions reading data into R.
+# - read.table, read.csv, for reading tabular data
+# - readLines, for reading lines of a text file
+# - source, for reading in R code files (inverse of dump)
+# - dget, for reading in R code files (inverse of dput)
+# - load, for reading in saved workspaces
+# - unserialize, for reading single R objects
+
+#There are analogous functions for writing data to files
+# - write.table, for writing tabular data to text files (i.e. CSV) or connections
+# - writeLines, for writing character data line-by-line to a file or connection
+# - dump, for dumping a textual representation of multiple R objects
+# - dput, for outputting a textual representation of an R object
+# - save, for saving an arbitrary number of R objects in binary format (possibly compressed)
+#   to a file.
+# - serialize, for converting an R object into a binary format for outputting to a connection (or file).
+
+#We will use here the files: "PETR4.txt", "SPETR4.txt", "GOLL4.txt"
+#Set the working directory where the files are or use the complete location when read it
+rm(list = ls())
+setwd("D:/$github/data_analysis_group/Introduction to R/2. Getting Data In and Out")
+getwd()
+dir()
+
+
+##READING DATA FILES WITH read.table()
+#Arguments:
+# - file, the name of a file, or a connection
+# - header, logical indicating if the file has a header line
+# - sep, a string indicating how the columns are separated
+# - colClasses, a character vector indicating the class of each column in the dataset
+# - nrows, the number of rows in the dataset. By default read.table() reads an entire file.
+# - comment.char, a character string indicating the comment character. This defalts to "#".
+#   If there are no commented lines in your file, it's worth setting this to be the empty string "".
+# - skip, the number of lines to skip from the beginning
+# - stringsAsFactors, should character variables be coded as factors? This defaults to TRUE.
+
+#Read the file SPETR4.txt with the function read.table()
+#this file contains two columns, date and closed price for PETR4 quotation
+data <- read.table("SPETR4.txt")
+data
+head(data)
+tail(data)
+rm(data)
+
+#For large data sets we can optimize the function inserting the argument colClasses - a vector
+#specifying the classes of the columns:
+initial <- read.table("SPETR4.txt", nrows = 100)
+classes <- sapply(initial, class) #the function sapply will apply class() over all columns (vectors) of initial
+rm(initial)
+#We created a vector of the classes of the columns of SPETR4.txt reading just 100 rows of the file
+#(don't need too much memory for this), now let's read the entire data using this vector:
+tabAll <- read.table("SPETR4.txt", colClasses = classes)
+tabAll
+head(tabAll)
+tail(tabAll)
+#That's it, the same data frame, but when working with big datas reading the file can be
+#optimized with the argument colClasses
+rm(tabAll, classes)
+
+#to use another read.'' you can use the same idea but the args and the defaults changes
+?read.table
+
+
+##USING THE 'readr' PACKAGE
+install.packages("readr")
+#This package have some functions for reading files that are much faster than base R analogues functions 
+#and provide a nice feature for progress meter.
+#The analogous functions in readr are read_table(), read_csv() and read_csv2().
+library("readr")
+data <- read_table("SPETR4.txt")
+data
+head(data)
+tail(data)
+rm(data)
+
+
+#A little more complex read
+read.table("PETR4.txt")
+readLines("PETR4.txt",10)
+PETR <- read.table("PETR4.txt"
+                   , header = T
+                   , sep=";"
+                   , dec=","
+                   , stringsAsFactors = F
+                   , skip = 4
+                   , na.strings = "-")
+class(PETR)
+head(PETR)
+
+
+
+###   QUESTIONS   ###
+
+#How to use the memory calculations more efficiently?
+    #In general, when using R with larger datasets, it's also useful to know a 
+    #few things about your system.
+    # - How much memory is available on your system?
+    # - What other applications are in use? Can you close any of them?
+    # - Are there other users logged into the same system?
+    # - What operating system are you using? Some operating systems can limit the amount of 
+    #   memory a single process can access
+
+
+#Make the topic below cleaner:
+    ##INTERFACES TO THE OUTSIDE WORLD
+    #Data are read in using connection interfaces. Connections can be made to files 
+    #(most common) or to other more exotic things.
+    # - file, opens a connection to a file
+    # - gzfile, opens a connection to a file compressed with gzip
+    # - bzfile, opens a connection to a file compressed with bzip2
+    # - url, opens a connection to a webpage
+
+    #File Connections
+    #file() function arguments:
+    #- description is the name of the file
+    #- open is a code indicating what mode the file should be opened in
+    #The open argument allows for the following options:
+    #- "r" open file in read only mode
+    #- "w" open a file for writing (and initializing a new file)
+    #- "a" open a file for appending
+    #- "rb", "wb", "ab" reading, writing, or appending in binary mode (Windows)
+    
+    
+    ## Create a connection to 'SPETR4.txt'
+    con <- file("SPETR4.txt")
+    ## Open connection to 'SPETR4.txt' in read-only mode
+    open(con, "r")
+    ## Read from the connection
+    data <- read.table(con)
+    ## Close the connection
+    close(con)
+    #which is the same as
+    #data <- read.table("SPETR4.txt")
+    rm(data,con)
+    
+    
+    #Reading Lines of a Text File
+    #Text files can be read line by line using the readLines() function.
+    #This function is useful for reading text files that may be unstructured or contain 
+    #non-standard data.
+    
+    
+    #Reading From a URL Connection
+    #The readLines() function can be useful for reading in lines of webpages. 
+    #Since web pages are basically text files that are stored on a remote server, 
+    #there is conceptually not much difference between a web page and a local text file. 
+    #However, we need R to negotiate the communication between your computer and the web server.
+    #This is what the url() function can do for you, by creating a url connection to a web server.
+    #This code might take time depending on your connection speed.
+    
+    ## Open a URL connection for reading
+    con <- url("https://www5.usp.br/", "r")
+    ## Read the web page
+    x <- readLines(con)
+    ## Print out the first few lines
+    head(x, 1)
+    close(con)
+    #While reading in a simple web page is sometimes useful, 
+    #particularly if data are embedded in the web page somewhere.
+    #However, more commonly we can use URL connection to read in specific data files 
+    #that are stored on web servers. Using URL connections can be useful for producing a 
+    #reproducible analysis, because the code essentially documents where the data came 
+    #from and how they were obtained. This is approach is preferable to opening a web browser
+    #and downloading a dataset by hand. Of course, the code you write with connections 
+    #may not be executable at a later date if things on the server side are changed or 
+    #reorganized.
+    rm(list = ls())
+    
---
+++ Introduction to R/3. Managing Data and Missing Values/3_managing_and_nas.R
@@ -0,0 +1,240 @@
+# Introduction to R #
+
+#3. MANAGING DATA AND MISSING VALUES___________________
+
+#There are 3 operators that can be used to subset R objects:
+# - "[]"    - always returns the same class of the original object;
+#             extract multiple elements.
+# - "[[]]"  - subset a list or a data frame;
+#             only extract a single element;
+#             class will not necessarily be the same of the original object.
+# - "$"     - subset elements of a data frame or list by literal name.
+
+
+##Subsetting a Vector
+x <- c(sample(LETTERS,6,replace=T))
+# Extract some elements
+x[1]
+x[2:4]
+x[c(1, 3, 5)]
+u <- x > "E"
+u
+x[u]
+x[x > "E"]
+rm(x,u)
+
+##Subsetting a Matrix
+x <- matrix(rnorm(20), 4, 5)
+x
+x[1, 2]   # the elements are [row,column]
+x[2, 1]
+x[1, ]    # extract the first row for all columns
+x[, 2]    # extract the second column for all rows
+#Dropping matrix dimensions
+#By default a matrix subsetting return a vector, to cancel this default
+#set the argument drop = FALSE, and it return a matrix
+x[1, 2, drop = FALSE]
+x[1, , drop = FALSE]
+
+
+##Read the data "PETR4.txt"
+rm(list = ls())
+setwd("D:/$github/data_analysis_group/Introduction to R/2. Getting Data In and Out")
+getwd()
+dir()
+PETR <- read.table("PETR4.txt"
+                   , header = T
+                   , sep=";"
+                   , dec=","
+                   , stringsAsFactors = F
+                   , skip = 4
+                   , na.strings = "-")
+
+#Use some functions for some data summary:
+head(PETR)
+tail(PETR)
+str(PETR)
+names(PETR)
+
+#We'll use only the columns 1,2,3,13,14 and 16. Filter it.
+PETR <- PETR[,c(1,2,3,13,14,16)]
+names(PETR)
+#replace the all columns names
+colnames(PETR) <- c("date", "close", "open", "return", "returnUS", "liquidity")
+names(PETR)
+head(PETR)
+#drop the 3rd column ("open")
+PETR <- PETR[,-3]
+
+#Do the same for GOLL4.txt
+GOL <- read.table("GOLL4.txt"
+                  , header = T
+                  , sep=";"
+                  , dec=","
+                  , stringsAsFactors = F
+                  , skip = 4
+                  , na.strings = "-")
+GOL <- GOL[,c(1,2,13,14,16)]
+colnames(GOL) <- c("date", "close", "return", "returnUS", "liquidity")
+head(GOL)
+tail(GOL)
+#The initial observations are missing, look for the begining of non missing values
+head(GOL[!is.na(GOL[,2]),])
+
+#Summary analysis
+str(PETR)
+str(GOL)
+#Use sapply to coerce the columns close, return, returnUS, liquidity to numeric
+#and date as.Date
+PETR[c("close","return","returnUS","liquidity")] <- sapply(PETR[c("close","return","returnUS","liquidity")], type.convert, dec=",")
+PETR$date <- as.Date(PETR$date, format = "%d/%m/%Y")
+GOL[c("close","return","returnUS","liquidity")] <- sapply(GOL[c("close","return","returnUS","liquidity")], type.convert, dec=",")
+GOL$date <- as.Date(GOL$date, format = "%d/%m/%Y")
+str(PETR)
+str(GOL)
+summary(PETR)
+
+#Subset the GOL data from 01/01/2014 to 31/12/2014
+GOL[GOL[,"date"]=="2013-12-31",]
+GOL[GOL[,"date"]=="2014-12-31",]
+GOL <- GOL[2873:3133,]
+head(GOL)
+tail(GOL)
+
+#Subset PETR for the same period
+PETR[PETR[,"date"]=="2013-12-31",]
+PETR[PETR[,"date"]=="2014-12-31",]
+PETR <- PETR[7306:7566,]
+head(PETR)
+tail(PETR)
+
+#Subseting using "dplyr"
+install.packages("dplyr")
+library(dplyr)
+select(PETR, close)
+filter(PETR, return > 0)
+filter(PETR, date > "2014-06-01", date < "2014-06-30")
+
+#Put data frames into a list
+quotes <- list(GOL=GOL,PETR=PETR)
+names(quotes)
+#The subset operators can be used to extract elements from a list
+quotes[[1]]
+quotes[["PETR"]]
+quotes$PETR
+names(quotes[["PETR"]])
+names(quotes[["GOL"]])
+#Get the 2nd element of the 1st element (ie. close from GOL)
+quotes[[c(1, 2)]]
+quotes[[1]][[2]]
+quotes[[c("GOL", "close")]]
+quotes[["GOL"]][["close"]]
+
+#Data pooled
+#compute the id (ticker) to identify the ticker into the pooled data
+PETR$id <- "PETR"
+head(PETR)
+GOL$id <- "GOL"
+head(GOL)
+
+pooled <- rbind(GOL,PETR)
+head(pooled)
+tail(pooled)
+pooled[pooled$id=="GOL",]
+
+
+#Pooled from a list
+install.packages("data.table")
+library("data.table")
+quotes[["PETR"]]["id"] <- "PETR"
+quotes[["GOL"]]["id"] <- "GOL"
+pooled2 <- rbindlist(quotes)
+head(pooled2)
+tail(pooled2)
+
+#Split pooled data into a list for different ticker
+quotes2 <- split(pooled, pooled$id)
+names(quotes2)
+
+
+
+##NAs
+rm(pooled,pooled2,quotes,quotes2,GOL)
+#How to work with NAs?
+#In general the functions have the argument "na.rm" to compute or not NAs on the process
+mean(PETR$close)
+mean(PETR$close, na.rm=T)
+
+#The function complete.cases() returns a logical vector indicating which cases are complete
+complete.cases(PETR)
+cPETR <- PETR[complete.cases(PETR),] #keep only lines that are complete
+
+#only "close" data complete cases (drop all lines that have NAs on the column "close"):
+closePETR <- PETR[complete.cases(PETR$close),]
+
+#or use the function na.omit()
+#it returns the object with listwise deletion of missing values.
+cPETR <- na.omit(PETR)
+
+
+#For a vector replacing with some value:
+PETR$close0 <- PETR$close
+PETR$close0[is.na(PETR$close0)] <- 0 #replace all NAs with 0
+PETR$close0
+
+PETR$closeM <- PETR$close
+PETR$closeM[is.na(PETR$closeM)] <- mean(PETR$close, na.rm = T) #replace all NAs with "close" mean
+PETR$closeM
+
+    #a function for replacing vectors NAs with 0:
+    na.zero <- function (x) {
+      x[is.na(x)] <- 0
+      return(x)
+    }
+PETR$close0 <- na.zero(PETR$close)
+tail(PETR)
+str(PETR)
+
+#Some functions to fill NAs
+library(zoo)
+#you can use the args from the fuction na.fill to choose how to fill NAs:
+#extend to indicate repetition of the leftmost or rightmost non-NA value
+PETR$closeE <- na.fill(PETR$close,"extend")
+tail(PETR)
+
+#use underlying time scale for interpolation
+closeI <- na.approx(PETR$close)
+#the interpolation cant fill the first and last values, so the vector returned has fewer length
+length(closeI)
+nrow(PETR)
+  #Its possible complete with NAs or other values:
+  closeI <- c(NA,closeI,0)
+  #or make the interpolation with na.rm = FALSE and keed NAs
+  closeI <- na.approx(PETR$close, na.rm = F)
+PETR$closeI <- closeI
+tail(PETR)
+str(PETR)
+summary(PETR)
+
+
+#Write out
+write.csv2(PETR, file = "PETR4.csv", row.names = F, dec = ",")
+
+
+
+
+###   QUESTIONS   ###
+
+#How to drop the name of the rows (index) when subsetting?
+    row.names(PETR) <- NULL
+    #Using NULL for the value resets the row names to seq_len(nrow(x)), regarded as 'automatic'.
+    row.names(PETR) <- seq_len(nrow(PETR)) #the same if use NULL
+
+#How to know the position of a logical subset?
+    #eg. I want to filter by this interval below without setting the rows references manualy:
+    PETR[PETR[,"date"]=="2013-12-31",]
+    PETR[PETR[,"date"]=="2014-12-31",]
+    PETR <- PETR[7306:7566,]
+    
+#If the median is minor than mean when fill NAs with interpolation it will get down the final mean?
+    
